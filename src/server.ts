839ec3b7e896219de4c8b3180839d52f82159ed1
PiotrStoklosa <PiotrJStoklosa@gmail.com>
2023-12-25 22:30:34 +0100

    Add endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,35 @@
-import express, { Request, Response } from 'express';
+import express, {Request, Response} from 'express';
+import {getAllCarpets, getCarpetById, getCarpetsByMaterial} from "./service/carpetService";
+import {createOrder} from "./service/orderService";
 
 const app = express();
 const port = 3001;
+require('./database/db')
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello from Express with TypeScript!');
+app.get('/carpet/:id', (req: Request, res: Response) => {
+    const carpetId = req.params.id;
+    res.send(getCarpetById(carpetId));
+});
+
+app.get('/carpet/:material', (req: Request, res: Response) => {
+    const carpetMaterial = req.params.material;
+    res.send(getCarpetsByMaterial(carpetMaterial));
+});
+
+app.get('/carpet', (_req: Request, res: Response) => {
+    res.send(getAllCarpets());
+});
+
+interface ProductRequest extends Request {
+    body: {
+        products: string[];
+    };
+}
+
+app.post('/order', (req: ProductRequest, res: Response) => {
+
+    const productList: string[] = req.body.products;
+    res.send(createOrder(productList));
 });
 
 app.listen(port, () => {
